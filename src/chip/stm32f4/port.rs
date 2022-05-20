0e0381a609897dd741d66e6024a3c99a3f27e0b6
高庆丰 <gao.qingfeng@gmail.com>
2022-05-20 10:05:48 +0800

    stm32f4

--- src/chip/stm32f4/port.rs
+++ src/chip/stm32f4/port.rs
@@ -1,84 +1,108 @@
 use core::arch::{asm, global_asm};
 
+use cortex_m::interrupt;
+
 use super::{SYSTICK_CLOCK_HZ, TICK_CLOCK_HZ};
 use crate::arch::cortex_m::rt::exception;
 use crate::isr_sprintln;
 use crate::task::scheduler;
 
-// // 导入汇编代码
-// global_asm!(include_str!("switch.S"));
+// 导入汇编代码
+// global_asm!(include_str!("port.S"));
 
 pub(crate) static mut SYSTICKS: u64 = 0;
 
+#[export_name = "print_sp"]
+unsafe fn print_sp(sp: usize) {
+    isr_sprintln!("sp {:#08x}", sp);
+}
 #[exception]
 unsafe fn SVCall() {
     isr_sprintln!("SVCall");
     asm!(
         "
-    ldr r3, = CURRENT_TASK_PTR
+    ldr r3, =CURRENT_TASK_PTR
     ldr r1, [r3]
     ldr r0, [r1]
-    ldmia r0!, {{r4-r11}}
+
+    ldmia r0!, {{r4-r11, r14}}
     msr psp, r0
-    isb 
-    cpsie i // 开中断
-    cpsie f // 开异常
-    dsb
     isb
-    # 返回任务模式，sp使用psp里的地址
-    orr r14, #0xd 
     # svcall异常返回，使用psp指向的地址出栈，也就是回到任务栈里了
-    bx r14
-    "
-    )
-}
-#[exception]
-unsafe fn PendSV() {
-    isr_sprintln!("PendSV");
-    asm!(
-        "
-    mrs r0, psp
-    isb
-    ldr r3, = CURRENT_TASK_PTR
-    ldr r2, [r3]
-
-    stmdb r0!, {{r4-r11}}
-    str r0, [r2]
-
-    stmdb sp!, {{r3,r14}}
-    cpsid i 
-    dsb
-    isb
-    bl switch_context
-    cpsie i 
-    dsb
-    isb
-    ldmia sp!, {{r3,r14}}
-
-    ldr r1, [r3]
-    ldr r0, [r1]
-    ldmia r0!, {{r4-r11}}
-    msr psp, r0
-    isb
+    mov r0, #0
+    msr basepri, r0
+    cpsie i //使能全局中断
+    cpsie f //使能全局异常
     bx r14
     nop
     "
     )
 }
+#[exception]
+unsafe fn PendSV() {
+    isr_sprintln!("PendSV begin");
+
+    asm!(
+        "
+        cpsid i
+        dsb
+        isb
+        bl print_sp
+        mrs r0, psp
+        isb
+        ldr r3, =CURRENT_TASK_PTR
+        ldr r2, [r3]
+    
+        tst r14, #0x10
+        it eq
+        vstmdbeq r0!, {{s16-s31}}
+    
+        stmdb r0!, {{r4-r11, r14}}
+        str r0, [r2]
+    
+        stmdb sp!, {{r0, r3}}
+
+        
+        
+        bl switch_context
+       
+        cpsie i
+        ldmia sp!, {{r0, r3}}
+    
+        ldr r3, =CURRENT_TASK_PTR
+        ldr r1, [r3]
+        ldr r0, [r1]
+    
+        ldmia r0!, {{r4-r11, r14}}
+    
+        tst r14, #0x10
+        it eq
+        vldmiaeq r0!, {{s16-s31}}
+    
+        msr psp, r0
+        isb
+    
+        bx r14
+        "
+    );
+}
 
 /// 系统节拍器中断
 #[exception]
 unsafe fn SysTick() {
-    isr_sprintln!("SysTick");
     const TICKS: u32 = SYSTICK_CLOCK_HZ as u32 / TICK_CLOCK_HZ as u32;
-    let tick = core::ptr::read_volatile(&SYSTICKS);
-    core::ptr::write_volatile(&mut SYSTICKS, tick + TICKS as u64);
-    scheduler::systick();
+    interrupt::free(|_| {
+        let tick = core::ptr::read_volatile(&SYSTICKS);
+        core::ptr::write_volatile(&mut SYSTICKS, tick + TICKS as u64);
+        scheduler::systick();
+        isr_sprintln!("SysTick");
+    });
 }
 
 /// 软中断切换任务
 #[export_name = "switch_context"]
-unsafe fn switch_context() {
+unsafe fn switch_context(sp: usize) {
+    isr_sprintln!("switch_context {:#08x}", sp);
     scheduler::schedule();
 }
 
