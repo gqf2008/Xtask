5e468ff2d514671643acef801dbe110ef1ca07ee
高庆丰 <gao.qingfeng@gmail.com>
2022-06-30 14:14:57 +0800

    cm32m4xxr hal

--- hal2/cm32m4xxr-hal/src/lib.rs
+++ hal2/cm32m4xxr-hal/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![no_std]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub use cm32m4xxr_pac as pac;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+use embedded_hal as hal;
